75b06e2e5515ecf575f449fdc72c99db59933da2
vmk <vm.kattenberg@gmail.com>
2016-08-09 17:38:44 +0200

    Added Hadoop mapreduce application to check dat files.

---
+++ src/main/java/nl/surfsara/hadoop/seti/VerifyDataArchives.java
@@ -0,0 +1,34 @@
+package nl.surfsara.hadoop.seti;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class VerifyDataArchives {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration conf = new Configuration();
+        conf.set("mapred.task.timeout", "7200000");
+
+        Job job = Job.getInstance(conf, "Verify data archives ...");
+        job.setJarByClass(VerifyDataArchives.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setMapperClass(VerifyMapper.class);
+        job.setReducerClass(Reducer.class);
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        job.waitForCompletion(true);
+    }
+
+}
